57f38a78adf960c9d0440ca1487ff9aff3fa00b9
Johnathan Lancelot <johnathan.lancelot@gmail.com>
2026-09-13 13:58:20 +1200

    Started making Angular components

--- eco-academy/src/app/app.component.ts
+++ eco-academy/src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { BackComponent } from './back/back.component';
+import { BlogComponent } from './blog/blog.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { HomeworkComponent } from './homework/homework.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [BackComponent, BlogComponent, FooterComponent, HomeComponent, HomeworkComponent,
+     RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'eco-academy';
+  title = 'Eco Academy';
 }
---
+++ eco-academy/src/app/back/back.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-back',
+  imports: [RouterLink],
+  templateUrl: './back.component.html',
+  styleUrl: './back.component.css'
+})
+export class BackComponent {
+
+}
